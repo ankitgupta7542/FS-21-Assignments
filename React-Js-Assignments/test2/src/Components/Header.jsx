b29c885a33dc5df90a7a1e4826e9b9d5b15691e9
ankitgupta7542 <ankitgupta7542@gmail.com>
2024-10-07 18:32:20 +0530

    changes added to the directory app

--- React-Js-Assignments/test2/src/Components/Header.jsx
+++ React-Js-Assignments/test2/src/Components/Header.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <>
-        <div style={{ height: "40px", backgroundColor: "green", justifyContent: "center", padding: "10px", display:"flex", alignItems:"center" }}>
-        <h2 style={{color:"whitesmoke"}}>Chirag's Directory App</h2>
+        <div style={{ height: "40px", backgroundColor: "darkyellow", justifyContent: "center", padding: "10px", display:"flex", alignItems:"center" }}>
+        <h2 style={{color:"whitesmoke"}}>Directory App</h2>
         </div>
-        <div style={{ height: "40px", backgroundColor: "lightgray", padding: "10px", display:"flex", alignItems:"center", justifyContent: "Space-evenly" }}>
+        <div style={{ height: "40px", backgroundColor: "lightyellow", padding: "10px", display:"flex", alignItems:"center", justifyContent: "Space-around", color:"white" }}>
             <button><Link to="/">Add new Person </Link></button>
             <button><Link to="/retrieve">Retrieve Information </Link></button>
         </div>
